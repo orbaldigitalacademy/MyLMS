972afb8cc7d8ba48609d15b3952464ec7e2409ed
Orbal Digital Academy <155068331+orbaldigitalacademy@users.noreply.github.com>
2026-07-15 06:39:04 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import { CurrencyProvider } from "./context/CurrencyContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CurrencyProvider>
+      <App />
+    </CurrencyProvider>
   </React.StrictMode>,
 );
