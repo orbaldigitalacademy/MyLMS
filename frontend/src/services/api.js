7c9ed1bc78dbe31c79998679070425ee675612d6
Orbal Digital Academy <155068331+orbaldigitalacademy@users.noreply.github.com>
2026-08-24 15:30:04 +0100

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,7 +8,7 @@ if (!API_URL) {
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
-  timeout: 150000,
+  timeout: 1000000,
 });
 
 console.log("BACKEND URL:", process.env.REACT_APP_BACKEND_URL);
